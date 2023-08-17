4854fe301765a7a4a2850014cc9a922a460624f5
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-08-18 00:10:39 +0530

    JWT Configuration

---
+++ server/src/main/java/com/yash/ecommerce/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.yash.ecommerce.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "ihvvbaddfvbabviaidfbviukdvbznckbafv";
+    public static final String JWT_HEADER = "Authorization";
+
+}
